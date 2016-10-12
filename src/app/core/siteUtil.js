3407816167927778d0f0cd9c93c2924ae240dca8
r95222023 <r95222023@gmail.com>
2016-10-12 08:03:22 -0500

    refactor siteUtil

--- src/app/core/siteUtil.js
+++ src/app/core/siteUtil.js
@@ -10,25 +10,19 @@
         module.exports = SiteUtil
     }
 
-
-    var siteNameRegEx=/#!\/(.*?)\//;
-    function SiteUtil(util){
-        this.util=util;
-        this.siteName='';
-        this.host=location.host;
-        var self=this;
+    function SiteUtil(util) {
+        this.util = util;
+        this.siteName = '';
+        this.domain='';
+        var self = this;
 
 
-        window.onhashchange= function(ev){
-            var newURL=ev.newURL;
+        window.onhashchange = function (ev) {
+            // if hostname is changed, the website will be reload automatically.
 
-            if(self.host&&self.host!==newURL.split('//')[1].split('/')[0]){
-                location.reload();
-            } else {
-                var match = location.hash.match(siteNameRegEx);
-                var siteName = match ? match[1] : '';
-                if(siteName&&siteName!==self.siteName) location.reload();
-            }
+            // check if siteName is changed. if so, reload.
+            var newSiteName =getSiteNameFromURL();
+            if (newSiteName&&newSiteName !== self.siteName) location.reload();
         };
     }
 
@@ -37,34 +31,34 @@
     SiteUtil.prototype.setSiteName = function (siteName) {
         this.siteName = siteName;
     };
-    SiteUtil.prototype.getSiteName = function(){
-        var self=this;
-        var url = location.href,
-            hostname = url.split('//')[1].split('/')[0];
 
+    function getSiteNameFromURL(){
+        var siteNameRegEx = /.*?\/\/.*?(\/#!\/|\/)(.*?)\//;
+        var match = location.href.match(siteNameRegEx);
+        var res = match ? match[2] : '';
+        return res&&res.search('#')===-1? res:'';
+    }
+
+    SiteUtil.prototype.getSiteName = function () {
+        var self = this;
         if (this.siteName) {
             return Promise.resolve(this.siteName);
-        } else if (location.hostname == 'localhost'||location.hostname.search('firebaseapp\.com') !== -1) {
-            var match = location.hash.match(siteNameRegEx);
-            this.siteName = match ? match[1] : '';
+        } else if (location.hostname === 'localhost' || location.hostname.search('firebaseapp\.com') !== -1) {
+            this.siteName = getSiteNameFromURL();
             return Promise.resolve(this.siteName);
         } else {
+            this.domain=location.host;
             return new Promise(function (resolve, reject) {
                 self.util.database.queryRef('sites', {
                     params: {type: 'list'},
                     orderBy: 'Child: domain',
-                    equalTo: hostname,
+                    equalTo: location.hostname,
                     limitToFirst: 1
                 }).once('child_added', function (snap) {
-                        var val = snap.val();
-                        if (!!val && val.siteName) {
-                            self.siteName = val.siteName;
-                            resolve(val.siteName);
-                        } else {
-                            self.siteName = url.split('#!/')[1].split('/')[0];
-                            resolve(self.siteName);
-                        }
-                    })
+                    var val = snap.val();
+                    self.siteName = val.siteName;
+                    resolve(val.siteName);
+                })
             })
         }
     };
@@ -73,55 +67,59 @@
         var self = this;
         return new Promise(function (resolve, reject) {
             self.getSiteName().then(function (siteName) {
-                siteCache[siteName] =siteCache[siteName]||{};
+                siteCache[siteName] = siteCache[siteName] || {};
                 if (siteCache[siteName].preload) {
                     resolve(siteCache[siteName].preload);
                 } else {
                     self.util.storage.getWithCache('site-config-preload?siteName=' + siteName).then(function (res) {
-                        siteCache[siteName].preload=res;
+                        siteCache[siteName].preload = res;
                         resolve(res);
                     }).catch(reject);
                 }
             })
         });
     };
-    SiteUtil.prototype.getPageName= function(){
+
+
+    SiteUtil.prototype.getPageName = function () {
         if (this.pageName) {
             return this.pageName;
-        } else if (location.href.search('localhost') !== -1||location.href.search('firebaseapp\.com') !== -1) {
-            var regEx = /#!\/.*?\/(.*?)\//;
-            var match = location.href.match(regEx);
-            return match ? match[1] : 'index'
+        } else if (location.href.search('localhost') !== -1 || location.href.search('firebaseapp\.com') !== -1) {
+            var pageNameRegEx = /.*?\/\/.*?(\/#!\/|\/).*?\/(.*?)\//;
+            var match = location.href.match(pageNameRegEx);
+            this.pageName=match ? match[2] : '';
+            return this.pageName
         } else {
             var url = location.href;
             return url.split('//')[1].split('/')[1];
         }
     };
-    SiteUtil.prototype.loadPage = function (_siteName,_pageName) {
+
+    SiteUtil.prototype.loadPage = function (_siteName, _pageName) {
         var self = this;
         return new Promise(function (resolve, reject) {
             self.getSiteName().then(function (SITENAME) {
-                var siteName = _siteName||SITENAME;
-                siteCache[siteName] =siteCache[siteName]||{};
-                siteCache[siteName].pageCache = siteCache[siteName].pageCache||{};
-                var pageName = _pageName||self.getPageName();
+                var siteName = _siteName || SITENAME;
+                siteCache[siteName] = siteCache[siteName] || {};
+                siteCache[siteName].pageCache = siteCache[siteName].pageCache || {};
+                var pageName = _pageName || self.getPageName();
                 if (siteCache[siteName].pageCache[pageName]) {
                     resolve(siteCache[siteName].pageCache[pageName]);
                 } else {
-                    self.util.storage.getWithCache('page?type=detail&id='+pageName+'&siteName=' + siteName).then(function (pageData) {
-                        var _pageData = pageData||{};
-                        var sources = _pageData.sources||[];
-                        self.util.loader.getExternalSourceUrls(sources, siteName).then(function(srcs){
-                            sources.forEach(function(source, index){
-                                if(source.src) {
+                    self.util.storage.getWithCache('page?type=detail&id=' + pageName + '&siteName=' + siteName).then(function (pageData) {
+                        var _pageData = pageData || {};
+                        var sources = _pageData.sources || [];
+                        self.util.loader.getExternalSourceUrls(sources, siteName).then(function (srcs) {
+                            sources.forEach(function (source, index) {
+                                if (source.src) {
                                     sources[index].src = srcs[index];
-                                } else if(source.href){
+                                } else if (source.href) {
                                     sources[index].href = srcs[index];
                                 }
                             });
-                            _pageData.sources= sources;
+                            _pageData.sources = sources;
                             resolve(_pageData);
-                            siteCache[siteName].pageCache[pageName]=_pageData;
+                            siteCache[siteName].pageCache[pageName] = _pageData;
                         });
                     }).catch(reject);
                 }
